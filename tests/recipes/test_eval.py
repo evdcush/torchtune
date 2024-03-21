d18a8186e6a9e61fb4c3d7a5086c7c3af37038e0
joecummings <jrcummings27@gmail.com>
2024-03-21 12:55:50 -0700

    Address testing

--- tests/recipes/test_eval.py
+++ tests/recipes/test_eval.py
@@ -15,15 +15,6 @@ import torchtune
 from tests.common import TUNE_PATH
 from torchtune import models
 
-
-pkg_path = Path(torchtune.__file__).parent.absolute()
-EVAL_CONFIG_PATH = Path.joinpath(
-    pkg_path, "_cli", "eval_configs", "default_eval_config.yaml"
-)
-
-# TODO - should probably go into a general directory
-from tests.recipes.utils import fetch_ckpt_model_path, llama2_small_test_ckpt
-
 models.small_test_ckpt_tune = llama2_small_test_ckpt
 
 
@@ -38,7 +29,7 @@ class TestEval:
 
         os.environ["CUDA_VISIBLE_DEVICES"] = ""
         cmd = f"""
-        tune eval \
+        tune eleuther_eval \
             --config {EVAL_CONFIG_PATH} \
             model._component_=torchtune.models.{model_ckpt} \
             model_checkpoint={ckpt_path} \
@@ -53,3 +44,18 @@ class TestEval:
 
         out_err = capsys.readouterr()
         assert "'acc,none': 0.3" in out_err.out
+
+    def test_eval_recipe_errors_without_lm_eval(self, capsys, monkeypatch):
+        cmd = f"""
+        tune eleuther_eval \
+            --config {EVAL_CONFIG_PATH} \
+            model._component_=torchtune.models.llama2.llama2_tokenizer \
+            tokenizer.path=/tmp/test-artifacts/tokenizer.model \
+            limit=10 \
+        """.split()
+        monkeypatch.setattr(sys, "argv", cmd)
+        with pytest.raises(SystemExit):
+            runpy.run_path(TUNE_PATH, run_name="__main__")
+
+        out_err = capsys.readouterr()
+        assert "Missing required argument" in out_err.err
